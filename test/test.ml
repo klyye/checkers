90ebce664e64c76048c3a2c5e055c8467fcaf214
Kyle Zhang <zkyle1337@gmail.com>
2023-07-21 00:58:26 -0700

    to/of 2d list test

--- test/test.ml
+++ test/test.ml
@@ -17,6 +17,23 @@ let board_tests =
            let p = Normal P1 in
            let b = put blank 4 7 p in
            assert_equal (Some p) (get b 4 7) ~printer:string_of_square );
+         ( "of and to 2d list" >:: fun _ ->
+           let n = None in
+           let p = Some (King P1) in
+           let q = Some (Normal P2) in
+           let lst =
+             [
+               [ n; p; q; n; p; q; n; p ];
+               [ p; q; n; p; q; n; p; q ];
+               [ q; n; p; q; n; p; q; n ];
+               [ n; n; n; n; n; n; n; n ];
+               [ q; q; q; q; q; q; q; q ];
+               [ p; p; p; p; p; p; p; p ];
+               [ n; n; n; n; p; p; p; q ];
+               [ p; p; p; p; q; q; n; n ];
+             ]
+           in
+           assert_equal lst (to_2d_list (of_2d_list lst)) );
        ]
 
 let _ = run_test_tt_main board_tests
