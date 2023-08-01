371645b1f9670dd3fd57318f007beede4ee1d083
Kyle Zhang <zkyle1337@gmail.com>
2023-08-01 13:26:05 -0700

    unbroke the change

--- lib/move.ml
+++ lib/move.ml
@@ -18,16 +18,15 @@ let string_of_move move =
   let jump = if move.is_jump then "jump" else "simple" in
   Printf.sprintf "(%d, %d) (%s, %s) %s" move.r move.c v_str h_str jump
 
-(* this is so fucking stupid that i have to go through this much effort just to make a set of moves
+(* this is so fucking stupid that i have to go through this much effort just to make a set of moves *)
+let int_of_dir dir =
+  let v, h = dir in
+  (match v with U -> 0 | D -> 1) + match h with L -> 0 | R -> 2
 
-   sort in a way such that moves towards the center are higher *)
 let compare m1 m2 =
   if m1.is_jump = m2.is_jump then
-    let adj_r1, adj_c1 = dest m1 in
-    let adj_r2, adj_c2 = dest m2 in
-    let dist1 = abs (adj_r1 - 4) + abs (adj_c1 - 4) in
-    let dist2 = abs (adj_r2 - 4) + abs (adj_c2 - 4) in
-    dist1 - dist2
+    if m1.dir = m2.dir then if m1.r = m2.r then m1.c - m2.c else m1.r - m2.r
+    else int_of_dir m1.dir - int_of_dir m2.dir
   else if m1.is_jump then 1
   else -1
 
